092b08d7ceb9acb2848032c5fa32f4d990a18ea7
Eric Benedetto <ericsplace3@gmail.com>
2021-05-09 13:49:25 -0400

    add new routes

--- src/index.js
+++ src/index.js
@@ -4,19 +4,50 @@ import './index.css';
 import { Login } from './components/Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AdminRoute } from './components/AdminRoute';
+import { StudentRoute } from './components/StudentRoute';
+import { FacultyRoute } from './components/FacultyRoute';
 import { FacultyView } from './components/FacultyView';
 import { Landing } from './components/Landing';
 import { CoursesView } from './components/CoursesView';
+import { GradesView } from './components/GradesView';
 import { StudentsView } from './components/StudentsView';
+import { EvaluationsView } from './components/EvaluationsView';
+import { EnrollmentView } from './components/EnrollmentView';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 ReactDOM.render(
     <Router>
         <Switch>
             <Route path='/login' component={Login} exact />
-            <AdminRoute path='/' component={Landing} exact />
+            <ProtectedRoute path='/' component={Landing} exact />
             <AdminRoute path='/faculty/all' component={FacultyView} exact />
             <AdminRoute path='/students/all' component={StudentsView} exact />
             <AdminRoute path='/courses/all' component={CoursesView} exact />
+            <AdminRoute
+                path='/evaluations/all'
+                component={EvaluationsView}
+                exact
+            />
+
+            <StudentRoute path='/enroll' component={EnrollmentView} exact />
+            <StudentRoute
+                path='/evaluations/create'
+                component={EvaluationsView}
+                exact
+            />
+            <StudentRoute path='/grades/view' component={GradesView} exact />
+
+            <FacultyRoute
+                path='/courses/teaching'
+                component={CoursesView}
+                exact
+            />
+            <FacultyRoute path='/grades/assign' component={GradesView} exact />
+            <FacultyRoute
+                path='/evaluations/view'
+                component={EvaluationsView}
+                exact
+            />
             <Login />
         </Switch>
     </Router>,
